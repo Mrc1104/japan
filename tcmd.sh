e4af4901c3d11dc736e25cef71ba0bfa49fe430b
Ryan Conaway <mc321015@ohio.edu>
2024-03-11 17:08:34 -0400

    Improves the compile-and-compare script

--- tcmd.sh
+++ tcmd.sh
@@ -8,17 +8,34 @@ handle_error(){
 
 trap 'handle_error $LINENO' ERR
 
+run1=4
+run2=4
+if [ $# -eq 1 ] 
+	then 
+		run1=$1
+		run2=$1
+	else	
+		run1=$1
+		run2=$2
+fi
+echo ${run1}
+echo ${run2}
+echo 
+echo "########## 0/3 ##########"
+echo
+make -C test_build
+
 echo 
 echo "########## 1/3 ##########"
 echo
-test_build/qwmockdatagenerator -r 4 -e 1:2000 --config qwparity_simple.conf --detectors mock_detectors.map --data testing/coda3/
+test_build/qwmockdatagenerator -r ${run1} -e 1:2000 --config qwparity_simple.conf --detectors mock_detectors.map --data testing/coda3/
 
 echo 
 echo "########## 2/3 ##########"
 echo
-test_build/qwparity -r 4 -e 1:2000 --config qwparity_simple.conf --detectors mock_detectors.map --data testing/coda3 --rootfiles testing/coda3
+test_build/qwparity -r ${run1} -e 1:2000 --config qwparity_simple.conf --detectors mock_detectors.map --data testing/coda3 --rootfiles testing/coda3
 
 echo 
 echo "########## 3/3 ##########"
 echo
-test_build/qwroot -l -q testing/compare.C
+test_build/qwroot -l -q 'testing/compare.C('${run1},${run2}')'
